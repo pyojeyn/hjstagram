ab7e49cd84d2b0e1a88daad1cce513c351f7dc70
pyojeyn <jeynpyo@gmail.com>
2021-11-25 23:05:19 +0900

    댓글 API 완

--- backend/src/api/comments/comment.ctrl.js
+++ backend/src/api/comments/comment.ctrl.js
@@ -33,7 +33,6 @@ export const write = async (ctx) => {
         },
     });
     console.log(comment);
-    console.log('왜 post 안담기니;;')
 
     try{
         await comment.save();
@@ -43,6 +42,7 @@ export const write = async (ctx) => {
     }
 }
 
+// 댓글 삭제
 export const remove = async (ctx) => {
     const { id } = ctx.params;
     try{
@@ -52,3 +52,28 @@ export const remove = async (ctx) => {
         ctx.throw(500,e);
     }
 }
+
+// 댓글 리스트 
+// GET /api/comments?postid=를 넘겨야 하나
+
+export const commentslist = async (ctx) => {
+    const { id } = ctx.query; //postid 넘겨야할듯...?
+    const query = {
+        ...(id ? {'post._id':id}:{}),
+    };
+
+    try{
+        const comments = await Comment.find(query)
+        .sort({_id:1})
+        .limit(10)
+        .lean()
+        .exec();
+
+        ctx.body = comments.map((comment) => ({
+            ...comment,
+            body:comment.body,
+        }));
+    }catch(e){
+        ctx.throw(500,e);
+    }
+};
