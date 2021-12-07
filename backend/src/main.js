5fa8b9a3647351635ea5dcb346931d4d7b0aa662
pyojeyn <jeynpyo@gmail.com>
2021-12-07 22:31:34 +0900

    합침

--- backend/src/main.js
+++ backend/src/main.js
@@ -6,7 +6,10 @@ import api from './api'
 import jwtMiddleware from './lib/jwtMiddleware';
 import serve from 'koa-static';
 import path from 'path';
-
+import http from 'http';
+import socketIO from 'socket.io';
+import fs from 'fs';
+import htmlRender from 'koa-html-render';
 
 
 const port = 4000;
@@ -15,12 +18,20 @@ const router = new Router();
 
 //미들웨어 등록!
 router.use('/api', api.routes());
+
+app.use(htmlRender());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
 app.use(serve(path.join(__dirname, 'uploads')));
+app.use(serve(path.join(__dirname, '../../frontend/build')));
 
-// app.use('/uploads', serve(path.join(__dirname, 'uploads')));
+
+router.get("/",async ctx=>{
+    await ctx.html("/../../frontend/build/index.html") // The meaning is to return to the client in the root directory of the project, in the static directory, in the html folder named demo.html
+});
+
+// 소켓소켓
 
 //MongoDB 연결
 mongoose
